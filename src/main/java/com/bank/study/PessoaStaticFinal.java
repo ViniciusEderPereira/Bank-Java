a83c714a3a94f511b46e21b200d62c3ed0be0186
Antonio <rodrigowserra@gmail.com>
2024-12-16 20:28:15 +0000

    class - static final

---
+++ src/main/java/com/bank/study/PessoaStaticFinal.java
@@ -0,0 +1,8 @@
+package com.bank.study;
+
+public class PessoaStaticFinal {
+
+	public static String nome;
+	public static final String sobreNome = "Pereira";
+	
+}
--- src/main/java/com/bank/study/TestMain.java
+++ src/main/java/com/bank/study/TestMain.java
@@ -6,6 +6,22 @@ import java.util.Scanner;
 
 public class TestMain {
 
+	public static void main(String[] args) {
+		
+		PessoaStaticFinal p1 = new PessoaStaticFinal();
+		p1.nome = "Antonio";
+		PessoaStaticFinal p2 = new PessoaStaticFinal();
+		p2.nome = "Vinicius";
+		
+		PessoaStaticFinal p3 = new PessoaStaticFinal();
+		
+		System.out.println(p1.nome);
+		System.out.println(p2.nome);
+		System.out.println(p3.nome);
+		System.out.println(PessoaStaticFinal.nome);
+		System.out.println(PessoaStaticFinal.sobreNome);
+		
+	}
 
 
 }
